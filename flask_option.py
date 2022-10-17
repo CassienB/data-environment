fce97b40473deddafe128bc85893e755849cf76d
cassien babey <cassien98@yahoo.fr>
2022-10-17 07:18:51 +0200

    finish

--- flask_option.py
+++ flask_option.py
@@ -4,7 +4,11 @@ import os
 
 def start():
     """returns the right message"""
-    pass  # YOUR CODE HERE
+    env = os.getenv('FLASK_ENV')
+    if env:
+        return f"Starting in {env} mode..."
+    return "Starting in empty mode..."
+
 
 if __name__ == "__main__":
     print(start())
